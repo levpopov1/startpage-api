1a0baa48ce61724548d3063061037f11d3b5185b
Lev Popov <levpopov1@gmail.com>
2023-03-21 15:34:29 +0000

    remove unused things

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
-import http from "http";
 import express, { json } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import morgan from "morgan";
-import { ApolloServer } from "apollo-server-express";
-import {
-  ApolloServerPluginDrainHttpServer,
-  ApolloServerPluginLandingPageLocalDefault,
-} from "apollo-server-core";
 import routes from "./routes";
 import connectDB from "./db";
-import { Schema } from "./graphql/schema";
-import { Resolvers } from "./graphql/resolvers";
-import PeopleAPI from "./dataSources/People";
 
 // sets environment variables based on centents of .env file
 dotenv.config();
@@ -42,30 +33,8 @@ app.use("/api", routes);
 // // begin listening on given port
 const PORT = process.env.PORT || 5000;
 
-async function startApolloServer() {
-  const httpServer = http.createServer(app);
-  const server = new ApolloServer({
-    typeDefs: Schema,
-    resolvers: Resolvers,
-    dataSources: () => {
-      return {
-        peopleAPI: new PeopleAPI(),
-      };
-    },
-    csrfPrevention: true,
-    cache: "bounded",
-    plugins: [
-      ApolloServerPluginDrainHttpServer({ httpServer }),
-      ApolloServerPluginLandingPageLocalDefault({ embed: true }),
-    ],
-  });
-  await server.start();
-  server.applyMiddleware({ app });
-  httpServer.listen({ port: PORT }, () => {
-    console.log(
-      `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
-    );
-  });
-}
-
-startApolloServer();
+app.listen(PORT, function () {
+  console.log(
+    `The app is running in ${process.env.NODE_ENV} mode on port: ${PORT}`
+  );
+});
