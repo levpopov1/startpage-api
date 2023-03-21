876ac2d0fa8d4d5167a099bfc20238db144b3c25
Lev Popov <levpopov1@gmail.com>
2023-03-21 23:26:40 +0000

    removing model for now

--- src/model/Person.ts
+++ src/model/Person.ts
@@ -1,30 +1,30 @@
-import { Schema, model } from "mongoose";
+// import { Schema, model } from "mongoose";
 
-interface IPerson {
-  firstname: string;
-  surname: string;
-  gender: Gender;
-}
+// interface IPerson {
+//   firstname: string;
+//   surname: string;
+//   gender: Gender;
+// }
 
-enum Gender {
-  male = "male",
-  female = "female",
-}
+// enum Gender {
+//   male = "male",
+//   female = "female",
+// }
 
-const PersonSchema = new Schema<IPerson>({
-  firstname: {
-    type: String,
-    required: true,
-  },
-  surname: {
-    type: String,
-    required: true,
-  },
-  gender: {
-    type: String,
-    enum: Object.values(Gender),
-    required: true,
-  },
-});
+// const PersonSchema = new Schema<IPerson>({
+//   firstname: {
+//     type: String,
+//     required: true,
+//   },
+//   surname: {
+//     type: String,
+//     required: true,
+//   },
+//   gender: {
+//     type: String,
+//     enum: Object.values(Gender),
+//     required: true,
+//   },
+// });
 
-export const Person = model<IPerson>("Person", PersonSchema, "people");
+// export const Person = model<IPerson>("Person", PersonSchema, "people");
