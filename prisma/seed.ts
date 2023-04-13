ceb4aa7b6bf163662935ceac9c729a5794c03770
Lev Popov <levpopov1@gmail.com>
2023-04-13 14:37:38 +0100

    generate profile and widget data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,22 +3,45 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const data = Array.from({ length: 20 }).map(() => {
+const seedData = Array.from({ length: 20 }).map(() => {
   const firstName = faker.name.firstName();
   const lastName = faker.name.lastName();
+  const widgets = Array.from({ length: 3 }).map(() => ({
+    name: faker.helpers.arrayElement(["imageOfTheDay", "search", "time"]),
+    column: Number(faker.random.numeric()),
+    row: Number(faker.random.numeric()),
+    width: Number(faker.random.numeric()),
+    height: Number(faker.random.numeric()),
+  }));
   return {
     email: faker.internet.email(firstName, lastName),
     userName: faker.internet.userName(firstName, lastName),
     firstName,
     lastName,
+    profile: {
+      create: {
+        widgets: {
+          create: widgets,
+        },
+      },
+    },
   };
 });
 
 async function main() {
   await prisma.user.deleteMany();
-  await prisma.user.createMany({
-    data,
-  });
+  await Promise.all(
+    seedData.map((item) =>
+      prisma.user.create({
+        data: item,
+      })
+    )
+  );
+
+  // cant use createMany with schemas that have relations
+  // await prisma.user.createMany({
+  //   data,
+  // });
 }
 
 main()
